27a2434bf796da667be3768d12e9906eeabfad45
FranticDreamer <edwardgmgmd@gmail.com>
2021-02-25 18:13:48 +0300

    Add PDBs into Copy Bat Files

--- copy-client.bat
+++ copy-client.bat
@@ -1,2 +1,3 @@
 rem Put a copy command in here to copy your client.dll into your mod directory
-robocopy ".\SDK\projects\Debug\client" "path-to-your-mod-folder\cl_dlls" client.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\SDK\projects\Debug\client" "E:\STEAMFILES\steamapps\common\Half-Life\trinity\cl_dlls" client.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\SDK\projects\Debug\client" "E:\STEAMFILES\steamapps\common\Half-Life\trinity\cl_dlls" client.pdb /njh /njs /ndl /nc /ns /np
--- copy-server.bat
+++ copy-server.bat
@@ -1,2 +1,3 @@
 rem Put a copy command in here to copy your hl.dll into your mod directory
-robocopy ".\SDK\projects\Debug\hl" "path-to-your-mod-folder\dlls" hl.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\SDK\projects\Debug\hl" "E:\STEAMFILES\steamapps\common\Half-Life\trinity\dlls" hl.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\SDK\projects\Debug\hl" "E:\STEAMFILES\steamapps\common\Half-Life\trinity\dlls" hl.pdb /njh /njs /ndl /nc /ns /np
